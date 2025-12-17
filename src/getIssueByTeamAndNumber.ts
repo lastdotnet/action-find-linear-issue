65dfbc96ae10a2b3606255c2602dd3c13603ba75
alphak3y <alphak3y@protonmail.com>
2025-12-17 06:41:17 -0700

    project id support

--- src/getIssueByTeamAndNumber.ts
+++ src/getIssueByTeamAndNumber.ts
@@ -5,24 +5,36 @@ import { Issue, LinearClient, Team } from "@linear/sdk";
  * @param linearClient LinearClient instance
  * @param team Linear team object
  * @param issueNumber Issue number to search for the "123" in "ENG-123"
+ * @param projectId Optional project ID to filter by
  * @returns The issue if it exists
  */
 const getIssueByTeamAndNumber = async (
   linearClient: LinearClient,
   team: Team,
   issueNumber: number,
+  projectId?: string,
 ): Promise<Issue | null> => {
-  const issues = await linearClient.issues({
-    filter: {
-      team: {
-        id: {
-          eq: team.id,
-        },
-      },
-      number: {
-        eq: issueNumber,
+  const filter: any = {
+    team: {
+      id: {
+        eq: team.id,
       },
     },
+    number: {
+      eq: issueNumber,
+    },
+  };
+
+  if (projectId) {
+    filter.project = {
+      id: {
+        eq: projectId,
+      },
+    };
+  }
+
+  const issues = await linearClient.issues({
+    filter,
   });
 
   if (issues.nodes.length === 0) {
